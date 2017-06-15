13f40ebf82c880cc4f1b2f8cec1f0dfa4504cba5
slava hatnuke <slava.hatnuke@gmail.com>
2017-06-15 23:48:23 +0300

    test

--- go.sh
+++ go.sh
@@ -1,4 +1,7 @@
 node -e "console.log(new Date())" > test.txt
+node -e "console.log(new Date())" > test-1.txt
+node -e "console.log(new Date())" > test-2.txt
+
 git add .
 git commit -m test
-git push --all
+## git push --all
