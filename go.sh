5d90c5f196c7c10c140de2eaabe1299d2f585335
slava hatnuke <slava.hatnuke@gmail.com>
2017-06-20 00:29:45 +0300

    #2 Issue 2

--- go.sh
+++ go.sh
@@ -3,5 +3,5 @@ node -e "console.log(new Date())" > test-1.txt
 node -e "console.log(new Date())" > test-2.txt
 
 git add .
-git commit -m "#1 Issue XXX"
-git push --all
+#git commit -m "#1 Issue XXX"
+#git push --all
