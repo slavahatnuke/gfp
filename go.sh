4e180ff4bcbe1827e4bce8960b91fd42a4a865e4
slava hatnuke <slava.hatnuke@gmail.com>
2017-12-28 01:56:44 +0200

    #448

--- go.sh
+++
@@ -1,7 +0,0 @@
-node -e "console.log(new Date())" > test.txt
-node -e "console.log(new Date())" > test-1.txt
-node -e "console.log(new Date())" > test-2.txt
-
-git add .
-git commit -m "#56"
-git push --all
